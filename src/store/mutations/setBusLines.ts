cb0185738810a44bcf0637d6c2ae6e9a512d637c
Mateusz Piątkowski <mateusz.piatkowski666@gmail.com>
2024-01-30 22:48:49 +0100

    fix ts error

--- src/store/mutations/setBusLines.ts
+++ src/store/mutations/setBusLines.ts
@@ -1,5 +1,5 @@
 import { BusLineModel } from '@/model/BusLine';
 import { StoreState } from '../state';
 
-export default (state: StoreState, lines: BusLineModel) =>
+export default (state: StoreState, lines: BusLineModel[]) =>
   (state.lines = lines);
