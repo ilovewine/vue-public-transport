5290b2fd98bc139f4171fe1b7f72c7b3851e7637
Mateusz Piątkowski <mateusz.piatkowski666@gmail.com>
2024-01-30 23:08:51 +0100

    add ActionContext typing

--- src/store/actions/index.ts
+++ src/store/actions/index.ts
@@ -1,7 +1,15 @@
+import { Commit, Dispatch } from 'vuex';
 import initializeData from './initializeData';
+import { StoreState } from '../state';
 
 export const enum ACTION {
   INITIALIZE_DATA = 'INITIALIZE_DATA',
+}
+
+export interface ActionContext {
+  commit: Commit;
+  dispatch: Dispatch;
+  state: StoreState;
 }
 
 export default {
--- src/store/actions/initializeData.ts
+++ src/store/actions/initializeData.ts
@@ -1,11 +1,12 @@
 import { ServerResponseModel } from '@/model/response';
 import axios from 'axios';
+import { ActionContext } from '.';
 
 const ENDPOINT_URL = `${import.meta.env.VITE_API_URL}/${
   import.meta.env.VITE_API_STOPS_PATH
 }`;
 
-export default async (context) => {
+export default async (context: ActionContext) => {
   const response = (await axios.get(
     ENDPOINT_URL
   )) satisfies ServerResponseModel;
