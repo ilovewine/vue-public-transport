2309f14b5b192b05b69b6f53ee6fb7c66348ed18
Mateusz Piątkowski <mateusz.piatkowski666@gmail.com>
2024-01-30 23:01:55 +0100

    move endpoint data to .env

--- src/store/actions/initializeData.ts
+++ src/store/actions/initializeData.ts
@@ -1,9 +1,13 @@
 import { ServerResponseModel } from '@/model/response';
 import axios from 'axios';
 
+const ENDPOINT_URL = `${import.meta.env.VITE_API_URL}/${
+  import.meta.env.VITE_API_STOPS_PATH
+}`;
+
 export default async (context) => {
   const response = (await axios.get(
-    'http://localhost:3000/stops'
+    ENDPOINT_URL
   )) satisfies ServerResponseModel;
   console.log(response);
 };
