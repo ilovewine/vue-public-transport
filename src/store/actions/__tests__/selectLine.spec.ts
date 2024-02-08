840726cc038e859cd7fb06f03f0d50ea525ed88f
Mateusz Piątkowski <mateusz.piatkowski666@gmail.com>
2024-02-08 19:27:04 +0100

    add test for selectedLine

---
+++ src/store/actions/__tests__/selectLine.spec.ts
@@ -0,0 +1,34 @@
+import { describe, it, expect } from 'vitest';
+import actions, { ACTION, Context } from '..';
+import withContext from './withContext';
+import { BusLineModel } from '@/types/BusLineModel';
+import Sortable from '@/class/Sortable';
+import { MUTATION } from '@/store/mutations';
+import { EMPTY_STOP } from '@/store/state';
+
+describe('selectLine', () => {
+  const selectLine = actions[ACTION.SELECT_LINE];
+
+  withContext((context) => {
+    it('should set current line and reset current stop', () => {
+      const line: BusLineModel = {
+        line: 1,
+        stops: new Sortable(),
+      };
+
+      selectLine(context as unknown as Context, line);
+
+      expect(context.commit).toHaveBeenCalledTimes(2);
+      expect(context.commit).toHaveBeenNthCalledWith(
+        1,
+        MUTATION.SET_CURRENT_LINE,
+        line
+      );
+      expect(context.commit).toHaveBeenNthCalledWith(
+        2,
+        MUTATION.SET_CURRENT_STOP,
+        EMPTY_STOP
+      );
+    });
+  });
+});
