035f840fe4cc3bdccd4bd306dba1b8f671592116
Mateusz Piątkowski <mateusz.piatkowski666@gmail.com>
2024-04-23 19:09:05 +0200

    append data as json instead of server

--- src/store/actions/initializeData.ts
+++ src/store/actions/initializeData.ts
@@ -1,18 +1,12 @@
 import { ServerResponseModel } from '@/types/response';
-import axios from 'axios';
 import { ACTION, Context } from '.';
 import { MUTATION } from '../mutations';
 import retrieveStops from './utils/retrieveStops';
 import retrieveLines from './utils/retrieveLines';
-
-const ENDPOINT_URL = `${import.meta.env.VITE_API_URL}/${
-  import.meta.env.VITE_API_STOPS_PATH
-}`;
+import { stops } from '/data.json';
 
 export default async (context: Context) => {
-  const response: Awaited<ServerResponseModel> = (await axios.get(ENDPOINT_URL))
-    .data;
-
+  const response = stops as ServerResponseModel;
   context.commit(MUTATION.SET_BUS_STOPS, retrieveStops(response));
   context.commit(MUTATION.SET_BUS_LINES, retrieveLines(response));
 
