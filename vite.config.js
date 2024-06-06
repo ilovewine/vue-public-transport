5e7a533d70bca97c69cff113f84a526793e6d873
Mateusz Piątkowski <mateusz.piatkowski666@gmail.com>
2024-06-06 22:59:29 +0200

    add base to vite config

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,13 @@ import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: '/vue-public-transport/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
     },
   },
-  // css: {
-  //   preprocessorOptions: {
-  //     scss: {
-  //       additionalData: `@import "@/scss/_variables.scss";`,
-  //     },
-  //   },
-  // },
   test: {
     environment: 'jsdom',
     includeSource: ['src/**/*.ts'],
